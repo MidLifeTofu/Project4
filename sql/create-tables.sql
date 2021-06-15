56b4a4a9ac3f4fa81e4011fd9eefbb04087ac746
Jay <jgporter87@outlook.com>
2021-06-15 21:10:23 +1000

    updated values in database, corrected seed-tables with new values

--- sql/create-tables.sql
+++ sql/create-tables.sql
@@ -2,14 +2,17 @@ DROP TABLE IF NOT EXISTS users,
 
 CREATE TABLE IF NOT EXISTS users (
     id SERIAL PRIMARY KEY,
+    first_name VARCHAR(255) NOT NULL,
+    surname VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL
 );
 
-DROP TABLE IF NOT EXISTS schedules (
+DROP TABLE IF NOT EXISTS schedules,
+
+CREATE TABLE schedules (
     id SERIAL PRIMARY KEY,
-    user_ID NOT NULL INT,
+    user_ID NOT NULL INTEGER,
     day INTEGER NOT NULL CHECK (day >= 1 AND day <= 7),
     start_at TIME NOT NULL,
     end_at TIME NOT NULL
