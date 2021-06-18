4217aa873f4dbf8c25d553b68e2667f87ee724eb
James Summergreene <james.summergreene@gmail.com>
2021-06-18 12:38:25 +1000

    Added extra routes, view engine set up, dependancies

--- index.js
+++ index.js
@@ -1,35 +1,63 @@
 const express = require('express')
+const morgan = require('morgan')
+const expressLayouts = require('express-ejs-layouts')
+const session = require('express-session')
+
 const app = express()
-const PORT = process.env.PORT || 3000
+const db = require('./database')
+
 
 //postgres setup
-const db = require('./database')        // TODO: double check these paths
-const client = require('./database')
+const PORT = process.env.PORT || 3000
+
 
 //body parser for post requests
 app.use(express.json()) 
 app.use(express.urlencoded({extended: true}))
 
-//sets the template to ejs
+
+// Morgan setup
+app.set(morgan('dev'))
+
+
+// View Engine 
+app.set('views', './views')
 app.set('view engine', 'ejs')
+app.use(expressLayouts)
+
 
 // sets the public folder to be the start path of public view files
 app.use(express.static('public'))
 
-// router files
+
+// session
+app.use(session({
+    cookie: {
+        maxAge: 1000 * 60 * 60
+    },
+    name: 'login_mrcoffee',
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+}))
+
+
+// Routes
+const homeRouter = require('./routes/home')
 const loginRouter = require('./routes/login')
 const signupRouter = require('./routes/signup')
+const logoutRouter = require('./routes/logout')
+const usersRouter = require('./routes/users')
+const errorRouter = require('./routes/error')
 
-// routes
+app.use('/', homeRouter)
+app.use('/users', usersRouter)
 app.use('/login', loginRouter)
 app.use('/signup', signupRouter)
-
-// LOGIN PAGE
-app.get('/', (req, res) => {
-    res.render('pages/login')
-})
+app.use('*', errorRouter)
 
 
+// PORT
 app.listen(PORT, () => {
     console.log(`App listening at http://localhost:${PORT}`)
 })
