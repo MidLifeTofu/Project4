4085fb08f9ee8336ed1928a2ca10518e9fab40db
James Summergreene <james.summergreene@gmail.com>
2021-06-18 13:02:49 +1000

    Fixed up database

--- sql/seed-tables.sql
+++ sql/seed-tables.sql
@@ -1,17 +1,17 @@
 INSERT INTO users(first_name, surname, email, password)
-VALUES (James, Bond, bondjames@gmail.com, spy1234)
+VALUES ('James', 'Bond', 'bondjames@gmail.com', 'spy1234');
 
 INSERT INTO users(first_name, surname, email, password)
-VALUES (Donald, Duck, donaldduck@gmail.com, daisy1234)
+VALUES ('Donald', 'Duck', 'donaldduck@gmail.com', 'daisy1234');
 
 INSERT INTO users(first_name, surname, email, password)
-VALUES (Mickey, Mouse, mickeymouse@gmail.com, minnie156234)
+VALUES ('Mickey', 'Mouse', 'mickeymouse@gmail.com', 'minnie156234');
 
 INSERT INTO schedules(user_id, day, start_at, end_at)
-VALUES (1, 4, 1000, 0330);
+VALUES (1, 4, '1000', '0330');
 
 INSERT INTO schedules(user_id, day, start_at, end_at)
-VALUES (1, 3, 0930, 0530);
+VALUES (1, 3, '0930', '0530');
 
 INSERT INTO schedules(user_id, day, start_at, end_at)
-VALUES (0, 1, 0930, 1200);
+VALUES (0, 1, '0930', '1200');
