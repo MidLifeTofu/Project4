3e9c904f78a5017057f1160b4050e4848960a399
James Summergreene <james.summergreene@gmail.com>
2021-06-18 12:48:28 +1000

    Added and updated existing routes to all pages, added classes to css for layout

--- routes/signup.js
+++ routes/signup.js
@@ -1,31 +1,61 @@
 const express = require('express')
-const router = express.Router()
 const db = require('../database')
+const router = express.Router()
 
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
+// Sign Up page
 router.get('/', (req, res) => {
     res.render('pages/signup', {
-        message: req.query.message
+        message: req.query.message,
+        title: 'Sign up'
     })
-
 })
 
 router.post('/', (req, res) => {
 
-    // TODO: validate form fields
-
+    // Validate fields so that they must require data
 
     // check whether password and confirm-password are the same
-    if (req.body.password != req.body.confirmPassword) {
-        res.redirect('/signup?message=Passwords%20do%20not%20match')
+    if (req.body.password != req.body.confirm-password) {
+        return res.redirect("/signup?message=Passwords%20don't%20match.")
     }
-    
-    console.log(req.body)
 
-    // TODO: check whether user already exists (check if email is already in db)
+    // Verify if user email exists, if it does continue with signup
+    db.oneOrNone('SELECT * FROM users WHERE email = $1;', [req.body.email])
+    .then((existingUser) => {
+        if (existingUser) {
+            return res.redirect("/signup?message=User%20already%20exists.")
+        }
+        else {
+            const newUser = {
+                email: req.body.email.toLowerCase(),
+                name: req.body.name,
+                password: bcrypt.hashSync(req.body.password, saltRounds)
+            }
 
+            db.none('INSERT INTO users(email, name, password) VALUES ($1, $2, $3);',
+            [newUser.email, newUser.name, newUser.password])
+            .then(() => {
+                console.log(newUser)
+                res.redirect('/login')
+            })
+            .catch((err) => {
+                // Error if user hasn't been inserted into database
+                const message = err.message.replace(/ /g, '%20')
+                res.redirect(`/singup?message=${message}`)
+            })
+        }
+    })
+    // Error if failed to check whether user email exists
+    .catch((err) => {
+        return res.send(err.message)
+    })
+})
 
-
-    res.redirect('/login')
+router.get('/success', (res, req) => {
+    res.render()
 })
 
 module.exports = router
