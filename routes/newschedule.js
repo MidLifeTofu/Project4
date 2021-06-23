2f1cadd9ded58d3d3f6471ed8627fcdda9d8eb71
James Summergreene <james.summergreene@gmail.com>
2021-06-23 18:24:57 +1000

    Added overlap feature

--- routes/newschedule.js
+++ routes/newschedule.js
@@ -22,27 +22,49 @@ router.post('/', (req, res) => {
         start_at: req.body.start_at,
         end_at: req.body.end_at,
     }
+
     if ((req.body.start_at || req.body.end_at) === '') {
         return res.redirect("/newschedule?message=Missing%20fields.")
     }
     if (req.body.start_at > req.body.end_at) {
         return res.redirect("/newschedule?message=Start%20time%20cannot%20be%20greater%20than%20end%20time.")
     }
-    // if (db.none('SELECT (start_at, end_at) VALUES ($1, $2) OVERLAPS * FROM schedules;', [newSchedule.start_at, newSchedule.end_at])) {
-
-    // }
-    else {
-         db.none('INSERT INTO schedules(user_id, day, start_at, end_at) VALUES ($1, $2, $3, $4);',
-        [newSchedule.user_id, newSchedule.day, newSchedule.start_at, newSchedule.end_at])
-        .then(() => {
-            res.redirect('/newschedule?message=Schedule%20added!')
-        })
-        .catch((err) => {
-            // Error if user hasn't been inserted into database
-            const message = err.message.replace(/ /g, '%20')
-            res.redirect(`/newschedule?message=${message}`)
-        })
-    }
+    db.any('SELECT * FROM schedules WHERE user_id = $1;',[newSchedule.user_id])
+    .then((data) => {
+        for (let i = 0; i < data.length; i++) {
+            console.log("Hello2")
+            db.any('SELECT (TIME $1, TIME $2) OVERLAPS (TIME $3, TIME $4);', [newSchedule.start_at, newSchedule.end_at, data[i].start_at, data[i].end_at])
+            .then((results) => {
+                if (results) {
+                    console.log("Hello4")
+                    console.log(results)
+                    return res.redirect("/newschedule?message=Schedule%20overlaps.")
+                }
+                else {
+                    console.log("Testing")
+                    db.none('INSERT INTO schedules(user_id, day, start_at, end_at) VALUES ($1, $2, $3, $4);', [newSchedule.user_id, newSchedule.day, newSchedule.start_at, newSchedule.end_at])
+                    .then(() => {
+                        return res.redirect('/newschedule?message=Schedule%20added!')
+                    })
+                    .catch((err) => {
+                        // Error if user hasn't been inserted into database
+                        const message = err.message.replace(/ /g, '%20')
+                        return res.redirect(`/newschedule?message=${message}`)
+                    })
+                }
+            })
+            .catch((err) => {
+                // Error if user hasn't been inserted into database
+                const message = err.message.replace(/ /g, '%20')
+                return res.redirect(`/newschedule?message=${message}`)
+            })
+        }
+    })
+    .catch((err) => {
+        // Error if user hasn't been inserted into database
+        const message = err.message.replace(/ /g, '%20')
+        return res.redirect(`/newschedule?message=${message}`)
+    })
 })
 
 module.exports = router
