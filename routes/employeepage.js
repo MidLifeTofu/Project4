1ab040db233686bd6a7dbc68b402d19453cca887
Jay <jgporter87@outlook.com>
2021-06-25 21:08:05 +1000

    added user data to appear on employye info page

--- routes/employeepage.js
+++ routes/employeepage.js
@@ -6,12 +6,16 @@ const { redirectToLogin } = require('../middlewear')
 router.get('/:id(\\d+)', redirectToLogin, (req, res) => {
     db.any('SELECT * FROM schedules where user_id = $1;', req.session.userId)
     .then((schedulesData) => {
+        db.any('SELECT * FROM users where id = $1;', req.session.userId)
+        .then((userData) => {
         res.render('pages/employeepage', {
             message: req.query.message,
             req: req,
             schedules: schedulesData,
+            users: userData,
             title: 'Employee Info'
         })
+    })
     })
 })
 
