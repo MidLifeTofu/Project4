5a8faebf604d6518c4a71d4de813db5afe411b6f
Jay <jgporter87@outlook.com>
2021-06-25 21:41:16 +1000

    tested and activated sign up validation

--- routes/employeepage.js
+++ routes/employeepage.js
@@ -6,7 +6,7 @@ const { redirectToLogin } = require('../middlewear')
 router.get('/:id(\\d+)', redirectToLogin, (req, res) => {
     db.any('SELECT * FROM schedules where user_id = $1;', req.session.userId)
     .then((schedulesData) => {
-        db.any('SELECT * FROM users where id = $1;', req.session.userId)
+        db.any('SELECT * FROM users where id = $1;',  req.session.userId)
         .then((userData) => {
         res.render('pages/employeepage', {
             message: req.query.message,
